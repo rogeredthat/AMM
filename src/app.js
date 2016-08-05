978cb00bd0b270b6eb323abc692ea7430bd71d3e
Abhilash Verma <vermaabhilash70@gmail.com>
2016-08-06 03:16:04 +0530

    iaa

--- src/app.js
+++ src/app.js
@@ -3,5 +3,5 @@ const {app, BrowserWindow} = electron;
 
 app.on('ready',() => {
 	let mainWindow = new BrowserWindow({width:800,height:600});
-	mainWindow.loadURL('https://google.com');
+	mainWindow.loadURL('file://C:/Users/Abhilash/Documents/AMM/src/ui/index.html');
 });
